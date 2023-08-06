cb30de3398d4594cfe4c372379a2a1e0f34c7fb4
minseokKim6823 <alstjr6823@naver.com>
2023-08-06 12:22:50 +0900

    react연습

---
+++ react-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import React from 'react';
+import {Routes, Route, Link} from "react-router-dom"
+import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Counter from './pages/Counter';
+import Input from './pages/Input';
+import Input2 from './pages/Input2';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>|
+        <Link to="/about">About</Link>|
+        <Link to="/counter">Counter</Link>|
+        <Link to="/Input">Input</Link>|
+        <Link to="/Input2">Input2</Link>|
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/counter" element={<Counter />}></Route>
+        <Route path="/Input" element={<Input />}></Route>
+        <Route path="/Input2" element={<Input2 />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/pages/Input2.js
@@ -0,0 +1,42 @@
+import React,{useState} from "react";
+
+const Input2 = () => {
+    const [inputs,setInputs] =useState({
+        name:"",
+        email:"",
+        tel:""
+    });
+
+    const {name,email,tel} =inputs;
+
+    const onChange = (e) => {
+        const value =e.target.value;
+        const id =e.target.id;
+
+        setInputs({
+            ...inputs,
+            [id]:value
+        })
+    };
+
+    return(
+        <div>
+            <div>
+                <label>이름</label>
+                <input type = "text" id ="name" value={name} onChange={onChange}/>
+            </div>
+            <div>
+                <label>이메일</label>
+                <input type = "text" id ="email" value={email} onChange={onChange}/>
+            </div>
+            <div>
+                <label>전화번호</label>
+                <input type = "text" id ="tel" value={tel} onChange={onChange}/>
+            </div>
+            <p>{name}</p>
+            <p>{email}</p>
+            <p>{tel}</p>
+        </div>
+    )
+}
+export default Input2;
